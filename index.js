993ee42a8bfe5258f4b3d313a5d582958cea741a
igor <igoratakhanov@gmail.com>
2019-11-13 11:42:16 -0500

    instance of car passing all tests

--- index.js
+++ index.js
@@ -74,9 +74,29 @@ Person.prototype.toString = function () {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.model = model
+  this.milesPerGallon = milesPerGallon
+  this.tank = 0
+  this.odometer = 0
 }
+
+Car.prototype.fill = function(gallons){
+  this.tank = this.tank + gallons
+}
+
+Car.prototype.drive = function(distance){
+  const drivableMiles = this.tank * this.milesPerGallon
+  if (distance >= drivableMiles ) {
+    this.odometer = this.odometer + drivableMiles
+    this.tank = 0
+    return `I ran out of fuel at ${this.odometer} miles!`
+  }
+  this.odometer = this.odometer + distance
+  this.tank = this.tank - (distance/this.milesPerGallon)
+  
+}
+
 
 /*
   TASK 3
