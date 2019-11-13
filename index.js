bba75c23b5ba1f45b49e10dc06604608d7e78549
igor <igoratakhanov@gmail.com>
2019-11-13 11:57:21 -0500

    Instances of Baby passing

--- index.js
+++ index.js
@@ -94,7 +94,6 @@ Car.prototype.drive = function(distance){
   }
   this.odometer = this.odometer + distance
   this.tank = this.tank - (distance/this.milesPerGallon)
-  
 }
 
 
@@ -105,10 +104,17 @@ Car.prototype.drive = function(distance){
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age);
+  this.favoriteToy = favoriteToy
+}
+Baby.prototype = Object.create(Person.prototype);
 
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`
 }
 
+console.log('ttt',Baby instanceof Person)
 /* 
   TASK 4
 
